9005b90d3d47f081fbbed0f19e91efc7d88b3009
andrew morton <drewish@katherinehouse.com>
2013-12-14 23:09:32 -0800

    Adding some docs and cleaning things up
    It's starting to feel like a real game.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pitcher, MIDI Pitch Training
+
+My wife bought me a MIDI keyboard for my birthday and I'm still not really
+comfortable playing it so I decided I'd try to write some programs to help me
+learn to play it.
+
+This program plays a note then waits for a key to be pressed. If you press the
+right one it plays a pleasing chord then picks a new note. If you guess
+incorrectly it plays a dissonant chord and lets you try again.
+
+Right now the only notes it tests you on are the C notes.
--- index.js
+++ index.js
@@ -1,96 +1,97 @@
-// Output
 var m = require('midi');
-var midi = require('midi-api')({ end: false });
 
 // Set up a new input.
 var input = new m.input();
 console.log(input.getPortName(0));
 input.openPort(0);
 
+// Output
 var output = new m.output();
 output.openPort(0);
-
+var midi = require('midi-api')({ end: false });
 midi.pipe(m.createWriteStream(output));
-midi.program(34);
 
+// This is the range of my keyboard
 var notes = [36, 48, 60, 72, 84, 96];
 var note;
-var timeout;
+var streak = 0;
 
+// Start state
 pick();
 
 function pick() {
-  // Don't give them the same note two times in a row.
   var index;
+  // Don't give them the same note two times in a row.
   do {
     index = Math.floor(Math.random() * notes.length);
   } while (note == notes[index]);
   note = notes[index];
 
-  console.log("note is " + note);
+  console.log("note:", note, "streak:", streak);
   setTimeout(play, 250);
 }
 
 function play() {
   midi.noteOn(note).rest(500).noteOff(note);
-  setTimeout(wait, 500);
+  // Let them start a little early if they know it.
+  setTimeout(wait, 250);
 }
 
 function wait() {
+  var timeout;
+
   function acceptGuess(deltaTime, message) {
     // Ignore note offs
     if (message[2] === 0) {
       input.once('message', acceptGuess);
       return;
     }
-
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-
-    if (message[1] == note) {
-      console.log('you got it!');
-      right();
-    }
     else {
-      console.log(message[1] + ' was close to ' + note + ', try again!');
-      wrong();
+      clearTimeout(timeout);
+
+      if (message[1] == note) {
+        right();
+      }
+      else {
+        wrong(message[1]);
+      }
     }
+  }
+
+  function timedout() {
+    input.removeListener('message', acceptGuess);
+    play();
   }
 
   // Accept a guess...
   input.once('message', acceptGuess);
   // ...but don't wait too long.
-  timeout = setTimeout(function() {
-    input.removeListener('message', acceptGuess);
-    play();
-  }, 5000);
+  timeout = setTimeout(timedout, 5000);
 }
 
 function right() {
+  streak += 1;
+
   midi
     .noteOn(note)
-//    .rest(50)
     .noteOn(note + 4)
     .noteOn(note - 3)
     .rest(100)
-    .noteOff(note)
-    .noteOff(note + 4)
-    .noteOff(note - 3)
+    .noteOff()
     ;
 
   setTimeout(pick, 500);
 }
 
-function wrong() {
+function wrong(picked) {
+  streak = 0;
+
   midi
-    .noteOn(note)
-    .noteOn(note + 1)
-    .noteOn(note - 1)
+    .noteOn(picked)
+    .noteOn(picked + 1)
+    .noteOn(picked - 1)
     .rest(250)
-    .noteOff(note)
-    .noteOff(note + 1)
-    .noteOff(note - 1)
+    .noteOff()
     ;
 
   setTimeout(play, 500);
